db5535404505f571823ed4888d144e150f882774
rommandz <rmandzyuk94@gmail.com>
2018-03-06 00:27:03 +0200

    add patterns task

--- main.js
+++ main.js
@@ -1,71 +1,131 @@
-//Task 1
-const promiseTimeout = (num) => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(), num);
-  });
-}
-
-promiseTimeout(2000).then(() => {
-  console.log('done from promiseTimeout');
-});
-
-//Task 2
-const div = document.querySelector('div');
-
-const income = [
-  {
-    direction: 'top',
-    elem: div,
-    px: 100,
-    delay: 1000
-  }, {
-    direction: 'left',
-    elem: div,
-    px: 200,
-    delay: 2000
-  }, {
-    direction: 'left',
-    elem: div,
-    px: -200,
-    delay: 500
-  }, {
-    direction: 'top',
-    elem: div,
-    px: -100,
-    delay: 1200
+// Module
+const Module = (function () {
+  let count = 0;
+  
+  return {
+    increment() {
+      return count++;
+    },
+    decrement() {
+      return count--;
+    },
+    reset() {
+      count = 0;
+    }
   }
-];
+})();
 
+//Singleton
+const Singleton = (function () {
+  let instance;
 
-const move = (arr) => {
-  console.log('start');
+  const initInstance = () => ({
+    name: 'Roman',
+    age: 23
+  });
 
-  let chaining = Promise.resolve();
-  const promises = arr
-    .map(({
-      direction,
-      elem,
-      px,
-      delay
-    }) => {
-      return function () {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            console.log(`Move ${direction} for ${px} px, whith ${delay} delay`);
-            elem.style[direction] = px.toString() + 'px';
-            resolve();
-          }, delay);
-        });
+  return {
+    getInstance() {
+      if (!instance) {
+        instance = initInstance();  
       }
-    })
-    .forEach(f => {
-      chaining = chaining.then(f);
-    });
 
-  return chaining;
+      return instance;
+    }
+  };
+})();
+
+//Facade
+const showSpinner = () => console.log('show spinner');
+const loadData = () => console.log('load data');
+const hideSpinner = () => console.log('hide spinner');
+const updateContent = () => console.log('update content');
+const setLog = () => console.log('set log');
+
+
+const Facade = {
+  update() {
+      showSpinner();
+      loadData();
+      hideSpinner();
+      updateContent();
+      setLog();
+  },
+  doSmthElse() {
+      // a lot of methods
+  }
 };
 
-move(income).then(() => {
-  div.style.background = 'rgb(70, 117, 69)';
-  console.log('done from move');
-});
+//Decotator
+class Dinner {
+  constructor() {
+    this.secondDish = 'meat whith potatos';  
+  }
+  menu() { 
+    return 'meat whith potatos';
+  } 
+}
+
+const salad = (dinner) => {
+  menu = dinner.menu();
+  dinner.menu = () => menu + ', cezar'; 
+}
+
+const beer = (dinner) => {
+  menu = dinner.menu();
+  dinner.menu = () => menu + ', Bud'; 
+}
+
+const dinner = new Dinner();
+salad(dinner);
+beer(dinner);
+
+//Factory
+class Car {
+  constructor() {
+      this.color = 'black';
+      this.doors = 4;
+  }
+  setColor(newColor) {
+      this.color = newColor;
+  }
+}
+
+class Sedan extends Car {
+  constructor() {
+      super();
+      this.doors = 4;
+  }
+}
+class Coupe extends Car {
+  constructor() {
+      super();
+      this.doors = 2;
+  }
+}
+
+class CarFactory {
+
+  createCar(settings) {
+    let car;
+
+    switch(settings.type) {
+      case 'sedan':
+        car = new Sedan();
+        break;
+      case 'coupe':
+        car = new Coupe();
+        break;
+      default:
+        car = new Car();
+        break;  
+    }
+    car.setColor(settings.color);
+
+    return car;
+  }
+}
+
+const carFactory = new CarFactory();
+const firstCar = carFactory.createCar({type: 'sedan', color: 'blue'});
+const secondCar = carFactory.createCar({type: 'coupe', color: 'red'});
